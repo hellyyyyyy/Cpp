91cd738de26d2386e0651093498b35cb5cc3f3bb
hellyyyyyy <134709785+hellyyyyyy@users.noreply.github.com>
2023-07-24 04:21:16 -0700

    Add files via upload

--- task3/p1.cpp
+++ task3/p1.cpp
@@ -2,45 +2,72 @@
 
 using namespace std;
 
-class Student{
-	private :
+class Employee{
 	
-	int id,salary,exp;
-	char name[100],role[100],add[100],email[100],contact[10];
+	private :
+		
+		int id,salary,exp;
+		char name[100],role[100],add[100],email[100],contact[100];
 	
 	public :
-		setData{int id,char name[],char role[],int salary, int exp, char add[], char email[],char contact[]}
-		{
-			this->id=id;
-			strcpy(this->name,name);
-			strcpy(this->role,role);
-			this->salary=salary;
-			this->exp=exp;
-			strcpy(this->add,add);
-			strcpy(this->email,email);
-			strcpy(this->contact,contact);
+		void setData(){
+			cout<<"ID : ";
+			cin>>id;
+			cout<<"Name : ";
+			cin>>name;
+			cout<<"Role : ";
+			cin>>role;
+			cout<<"Salary : ";
+			cin>>salary;
+			cout<<"Experience : ";
+			cin>>exp;
+			cout<<"Address : ";
+			cin>>add;
+			cout<<"E-mail : ";
+			cin>>email;
+			cout<<"Contact Number : ";
+			cin>>contact;
 		}
 		
-		getData(){
-			
-			cout<<"Student Id :"<<id<<endl;
-			cout<<"Student Name :"<<name<<endl;
-			cout<<"Student Role :"<<role<<endl;
-			cout<<"Student Salary :"<<salary<<endl;
-			cout<<"Student Experience :"<<exp<<endl;
-			cout<<"Student address :"<<add<<endl;
-			cout<<"Student E-mail :"<<email<<endl;
-			cout<<"Student Contact Number :"<<contact<<endl;
+		void getData(){
+			cout<<"ID : "<<id<<endl
+				<<"Name : "<<name<<endl
+				<<"Role : "<<role<<endl
+				<<"Salary : "<<salary<<endl
+				<<"Experience : "<<exp<<endl
+				<<"Address : "<<add<<endl
+				<<"E-mail : "<<email<<endl
+				<<"Contact Number : "<<contact<<endl;
 		}
-    };		
-
+		
+};
 
 int main(){
 	
-	Student s1,s2,s3,s4,s5;
+	Employee e1,e2,e3,e4,e5;
 	
-	s1.setData(int id,char name[],char role[],int salary, int exp, char add[], char email[],char contact[]);
-	s1.getdata();
+	cout<<"Enter Employee 1 Details:"<<endl;
+	e1.setData();
+	cout<<endl<<"Enter Employee 2 Details:"<<endl;
+	e2.setData();
+	cout<<endl<<"Enter Employee 3 Details:"<<endl;
+	e3.setData();
+	cout<<endl<<"Enter Employee 4 Details:"<<endl;
+	e4.setData();
+	cout<<endl<<"Enter Employee 5 Details:"<<endl;
+	e5.setData();
+	
+	cout<<endl<<"Employee 1 :"<<endl;
+	e1.getData();
+	cout<<endl<<"Employee 2 :"<<endl;
+	e2.getData();
+	cout<<endl<<"Employee 3 :"<<endl;
+	e3.getData();
+	cout<<endl<<"Employee 4 :"<<endl;
+	e4.getData();
+	cout<<endl<<"Employee 5 :"<<endl;
+	e5.getData();
+	
 	
 	return 0;
 }
